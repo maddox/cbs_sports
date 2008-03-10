c02d83e2d1dd23f3195b0a28d5697d25b3b4f191
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-03-09 21:58:35 -0400

    handling games that haven't started and their start time from epoch time

--- parse.rb
+++ parse.rb
@@ -29,20 +29,19 @@ def parse_game(game_html)
   when /left/
     game.started = true
     game.completed = false
-  else
+  when /(\d\d\d\d\d\d\d\d+)/
     game.started = false
     game.completed = false
+    game.time_left = Time.at($1.to_i).strftime('%I:%M %p %Z')
   end
+  
   
   game.team1 = Hpricot(game_html.to_s).at("//span div table tr:nth(1) td b").inner_html
   game.team2 = Hpricot(game_html.to_s).at("//span div table tr:nth(2) td b").inner_html
   
-  
-
   if game.started?
-    puts game.inspect
-    game.score1 = Hpricot(game_html.to_s).at("//span div table tr:nth(1) td:nth(3) b").innerHTML
-    game.score2 = Hpricot(game_html.to_s).at("//span div table tr:nth(2) td:nth(3) b").inner_html
+    game.score1 = Hpricot(game_html.to_s).at("//span div table tr:nth(1) td:last b").inner_html
+    game.score2 = Hpricot(game_html.to_s).at("//span div table tr:nth(2) td:last b").inner_html
   end
   
   game
@@ -50,11 +49,14 @@ def parse_game(game_html)
 end
 
 games = []
-#doc = Hpricot(open("data.html"))
-doc = Hpricot(open("http://www.sportsline.com/collegebasketball/scoreboard"))
+doc = Hpricot(open("data.html"))
+# doc = Hpricot(open("http://www.sportsline.com/collegebasketball/scoreboard", 'User-Agent' => 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)' ))
+
 
 doc.search("//span[@id*='board']").each do |game_html|
   games << parse_game(game_html)
 end
 
-puts games.class
+games.each do |game|
+  puts game.inspect
+end
